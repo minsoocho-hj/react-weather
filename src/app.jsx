ab47388375b465fe6d7abf28414eb111bda8cdde
minsoo cho <khg04170@gmail.com>
2022-04-28 17:52:20 +0100

    project setup

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,8 @@
-import './app.css';
+import './app.module.css';
+
+// 1. 유저는 현재기반 날씨 볼 수 있다(이름, 날씨, 온도)
+// 2. 유저는 주요도시 선택가능, 선택된 도시 이름, 날씨, 온도 볼 수 있다
+// 3. 날씨 로딩 중 로딩 스피너.
 
 function App() {
 	return <div className='App'>hi</div>;
