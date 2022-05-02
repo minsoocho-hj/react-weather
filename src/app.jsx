93ac5b546f086babe32a60c7c262b8d7bcf333a1
minsoo cho <khg04170@gmail.com>
2022-05-02 12:40:28 +0100

    reset all and made getCurrentLocationWeather display

--- src/app.jsx
+++ src/app.jsx
@@ -9,13 +9,8 @@ import SelectCityBox from './component/selectCityBox/selectCityBox';
 
 function App() {
 	const [weather, setWeather] = useState(null);
-	const [selectedCity, setSelectedCity] = useState('');
-	let [loading, setLoading] = useState(true);
-	let [color, setColor] = useState('#ffffff');
 
-	const cities = ['London', 'New york', 'Seoul', 'Bali'];
-
-	const getCurrentLocation = () => {
+	const getLocation = async () => {
 		navigator.geolocation.getCurrentPosition((position) => {
 			let lat = position.coords.latitude;
 			let lon = position.coords.longitude;
@@ -24,47 +19,37 @@ function App() {
 	};
 
 	const getWeatherByCurrentLocation = async (lat, lon) => {
-		let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=c97f834cfb3a81ae3e08b5747cf37011&=${process.env.REACT_APP_API_KEY}&units=metric`;
-		setLoading(true);
-		let response = await fetch(url);
-		let data = await response.json();
-		setWeather(data);
-		setLoading(false);
-	};
+		let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}&units=metric`;
 
-	const getWeatherByCity = async () => {
-		let url = `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&appid=${process.env.REACT_APP_API_KEY}&units=metric`;
-		setLoading(true);
-		let response = await fetch(url);
-		let data = await response.json();
-		setWeather(data);
-		setLoading(false);
+		await fetch(url)
+			.then((res) => res.json())
+			.then(
+				(data) => {
+					setWeather(data);
+				},
+				// Note: it's important to handle errors here
+				// instead of a catch() block so that we don't swallow
+				// exceptions from actual bugs in components.
+				(error) => {
+					console.log('error ' + error);
+				}
+			);
 	};
 
 	useEffect(() => {
-		if (selectedCity === '') {
-			getCurrentLocation();
-		} else {
-			getWeatherByCity();
-		}
-	}, [selectedCity]);
+		getLocation();
+	}, []);
 
 	return (
 		<>
-			{loading ? (
-				<ClipLoader color={color} loading={loading} size={100} />
-			) : (
-				<>
-					<div className={styles.weather}>
+			<div className='container'>
+				<div className='row'>
+					<div className='col-6 col-md-8 col-sm-10'>
 						<WeatherDisplay weather={weather} />
 					</div>
-					<SelectCityBox
-						setSelectedCity={setSelectedCity}
-						cities={cities}
-						selectedCity={selectedCity}
-					/>
-				</>
-			)}
+				</div>
+			</div>
+			{/* <SelectCityBox /> */}
 		</>
 	);
 }
--- src/component/weatherDisplay/weatherDisplay.jsx
+++ src/component/weatherDisplay/weatherDisplay.jsx
@@ -3,16 +3,21 @@ import styles from './weatherDisplay.module.css';
 
 const WeatherDisplay = ({ weather }) => {
 	return (
-		<div className={styles.display}>
-			<h1 className={styles.city}>{weather?.name}</h1>
-			<h2 className={styles.temperature}>{weather?.main.temp}°C</h2>
-			<div className={styles.weathers}>
+		<div className={styles.box}>
+			<h1 className={styles.city}>{weather && weather.name}</h1>
+			<h2 className={styles.temp}>{weather && weather.main.temp}°C</h2>
+			<div className={styles.weather}>
 				<img
-					src={`http://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`}
-					alt=''
 					className={styles.icon}
+					src={
+						weather &&
+						`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
+					}
+					alt='weather icon'
 				/>
-				<h2 className={styles.weather}>{weather?.weather[0].description}</h2>
+				<p className={styles.desc}>
+					{weather && weather.weather[0].description}
+				</p>
 			</div>
 		</div>
 	);
